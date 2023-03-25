32cfe33ee56130c9512157e52d0dbe342126141c
Pedro Canjura <67487163+pcanjura@users.noreply.github.com>
2023-03-25 13:45:34 -0600

    Add files via upload

---
+++ PracticasPY/inmobiliaria.py
@@ -0,0 +1,6 @@
+inmuebles = [{'año': 2000, 'metros': 100, 'habitaciones': 3, 'garaje': True, 'zona': 'A'},
+            {'año': 2012, 'metros': 60, 'habitaciones': 2, 'garaje': True, 'zona': 'B'},
+            {'año': 1980, 'metros': 120, 'habitaciones': 4, 'garaje': False, 'zona': 'A'},
+            {'año': 2005, 'metros': 75, 'habitaciones': 3, 'garaje': True, 'zona': 'B'},
+            {'año': 2015, 'metros': 90, 'habitaciones': 2, 'garaje': False, 'zona': 'A'}
+            ]
